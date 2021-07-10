698711f611ea211fd7869cca226c835a2a1294da
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-09 22:04:08 -0300

    refactor: extract components from App.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+import { Header } from './Header';
+
+import '../styles/content.scss';
+
+interface ContentProps {
+	movies: Array<{
+		imdbID: string;
+		Title: string;
+		Poster: string;
+		Ratings: Array<{
+			Source: string;
+			Value: string;
+		}>;
+		Runtime: string;
+	}>;
+	selectedGenre: {
+		title: string;
+	};
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+	return (
+		<div className="container">
+			<Header selectedGenreTitle={selectedGenre.title} />
+
+			<main>
+				<div className="movies-list">
+					{movies.map((movie) => (
+						<MovieCard
+							key={movie.imdbID}
+							title={movie.Title}
+							poster={movie.Poster}
+							runtime={movie.Runtime}
+							rating={movie.Ratings[0].Value}
+						/>
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
